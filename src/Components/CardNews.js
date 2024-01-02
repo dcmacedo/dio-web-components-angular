fd5bbc5087108a31c287401e4bf50b3dfa93f78b
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-01-02 18:38:41 -0300

    :sparkles: feat: conclusão do componente personalizado

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -19,6 +19,7 @@ class Cardnews extends HTMLElement{
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url") || "#"
 
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
@@ -31,6 +32,8 @@ class Cardnews extends HTMLElement{
         cardRight.setAttribute("class","card__right");
         
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "../assets/default-image.png"
+        newsImage.alt = "Darth Vader tomando café"
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -39,7 +42,45 @@ class Cardnews extends HTMLElement{
         return componentRoot;
     }
 
-    styles(){}
+    styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+                width: 80%;
+                box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+            }
+            
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+            
+            .card__left > span {
+                font-weight: 400;
+            }
+            
+            .card__left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+            
+            .card__left > p {
+                color: rgb(70, 70, 70);
+            }
+        `
+
+        return style;
+    }
 }
 
 customElements.define('card-news', Cardnews);
